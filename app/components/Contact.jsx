b9e3a0cb08485c74e46b3de82c6be238c55ed424
Radheshyam00 <radheshyamjanwa666@gmail.com>
2025-02-24 14:49:45 +0530

    fix security

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "951b7a4a-de36-49a2-88fb-96253c721e23");
+    formData.append("access_key", process.env.NEXT_PUBLIC_WEB3FORMS_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
