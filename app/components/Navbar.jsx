e24b3402effc6b943f612dc7e7bb12b2456c20b0
Radheshyam00 <radheshyamjanwa666@gmail.com>
2025-04-07 14:28:14 +0530

    fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
         <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 
             ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
             <a href='#top'>
-                <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
+                <Image src={assets.logo} alt='' className='md:block hidden w-28 cursor-pointer mr-14'/>
             </a>
             <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
                 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark: border dark:border-white/50 dark:bg-transparent"}`}>
