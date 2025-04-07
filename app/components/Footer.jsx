792c92796fa028a2ff939001a7ba9389ba73037e
Radheshyam00 <radheshyamjanwa666@gmail.com>
2025-04-07 15:08:44 +0530

    fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -20,15 +20,15 @@ const Footer = ({isDarkMode}) => {
                 >
                   <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300"/>
                   <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" /> 
-                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300"/>
-                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" /> 
-                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300"/>
-                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" /> 
-                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300"/>
-                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" /> 
+                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300" className='md:block hidden'/>
+                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" className='md:block hidden'/> 
+                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300" className='md:block hidden'/>
+                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" className='md:block hidden'/> 
+                  <img src="https://cyberdefenders-storage.s3.me-central-1.amazonaws.com/profile-badges/Pagalboy00.png" width="300" className='md:block hidden'/>
+                  <img src="https://tryhackme-badges.s3.amazonaws.com/dead.shut.png" alt="Your Image Badge" width="350" className='md:block hidden'/> 
                 </motion.div>
         </div>
-        <div className='flex flex-wrap gap-6'>
+        {/* <div className='flex flex-wrap gap-6'>
                 <motion.div
                     className="flex space-x-4 mt-4 gap-20"
                     animate={{ x: ["100%", "-100%"] }} // Moves from start to end
@@ -39,22 +39,22 @@ const Footer = ({isDarkMode}) => {
                     }}
                     whileHover={{ animationPlayState: "paused" }}
                 >
-                  <Image src={assets.ceh} alt='' className='w-36 mx-auto mb-2'/>
-                  <Image src={assets.CSA} alt='' className='w-36 mx-auto mb-2'/>
-                  <Image src={assets.Fabric} alt='' className='w-36 mx-auto mb-2'/>
-                  <Image src={assets.rhcsa} alt='' className='w-36 mx-auto mb-2'/>
-                  <Image src={assets.detect} alt='' className='w-36 mx-auto mb-2'/>
-                  <Image src={assets.cc} alt='' className='w-36 mx-auto mb-2'/>
+                  <Image src={assets.ceh} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
+                  <Image src={assets.CSA} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
+                  <Image src={assets.Fabric} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
+                  <Image src={assets.rhcsa} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
+                  <Image src={assets.detect} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
+                  <Image src={assets.cc} alt='' className='w-36 mx-auto mb-2 md:block hidden'/>
                 </motion.div>
-        </div>
-          {/* <div className='grid grid-cols-auto gap-6 my-10 px-20'>
+        </div> */}
+        <div className='grid grid-cols-auto gap-6 my-10 px-20'>
             <Image src={assets.ceh} alt='' className='w-36 mx-auto mb-2'/>
             <Image src={assets.CSA} alt='' className='w-36 mx-auto mb-2'/>
             <Image src={assets.Fabric} alt='' className='w-36 mx-auto mb-2'/>
             <Image src={assets.rhcsa} alt='' className='w-36 mx-auto mb-2'/>
             <Image src={assets.detect} alt='' className='w-36 mx-auto mb-2'/>
             <Image src={assets.cc} alt='' className='w-36 mx-auto mb-2'/>
-          </div> */}
+        </div>
             
             <div className='w-max flex items-center gap-2 mx-auto mt-10'>
                 <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
